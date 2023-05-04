38325310a0f46ad78787c7c2ec02b7c09ddfbf8f
akorfanusi <pomevorakorfa@gmail.com>
2023-05-04 13:42:04 -0700

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,10 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 
-	while (!(str[i] >= 'a' && str[i] <= 'z'))
-		i++;
-		if (str[i - 1] == ' ' ||
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		(str[i - 1] == ' ' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
 		str[i - 1] == ',' ||
